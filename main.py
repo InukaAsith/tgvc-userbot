1dc4d29b92ad933c58aa1aa18653cc924b333517
Inuka ASiTH <inukaasith7@gmail.com>
2021-03-15 19:29:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from pyrogram import Client as bot
 API_ID = int(environ["API_ID"])
 API_HASH = environ["API_HASH"]
 bot_token = environ["BOT_TOKEN"]
+session_name = environ["SESSION_NAME"]
 
 Plugins = dict(
     root="plugins",
     include=[
         "vc.player",
-        "ping",
         "sysinfo"
     ]
 )
@@ -20,8 +20,15 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=Plugins
+    plugins=plugins
 )
+
+app = Bot(session_name, API_ID, API_HASH)
+# logging.basicConfig(level=logging.INFO)
+app.start()
+print('>>> DPLAYER STARTED')
+
 
 bot.start()
 run()
+
